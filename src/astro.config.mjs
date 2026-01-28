ee8f0358071b9dc4e75291a44a37edd5ac6aeecc
Fuji <fujiji.entire1414@gmail.com>
2026-01-28 16:15:38 +0900

    prepare first article

--- src/astro.config.mjs
+++ src/astro.config.mjs
@@ -1,17 +1,15 @@
 // @ts-check
-
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap()],
-
   vite: {
-    plugins: [tailwindcss()],
+    plugins: [tailwindcss()]
   },
+
+  integrations: [mdx()]
 });
